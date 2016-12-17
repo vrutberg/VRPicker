aa3317e14e11594095f3530b5a838fadaacc0494
Viktor Rutberg <wishie@gmail.com>
2016-12-17 15:55:10 +0100

    Made the slider behave correctly.

--- Example/VRSlider/ViewController.swift
+++ Example/VRSlider/ViewController.swift
@@ -14,23 +14,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let containerView = UIView(frame: CGRect(x: 0, y: 0, width: 480, height: 640))
-        
-        view.addSubview(containerView)
-        
-        let config = VRSliderConfiguration(values: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], defaultSelectedIndex: 1, itemWidth: 100, sliderHeight: 100, sliderWidth: 480)
+        let config = VRSliderConfiguration(values: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], defaultSelectedIndex: 1, itemWidth: 100, sliderHeight: 100, sliderWidth: 414)
         
         let sliderView = VRSlider(configuration: config)
         
         sliderView.translatesAutoresizingMaskIntoConstraints = false
         
-        containerView.addSubview(sliderView)
+        view.addSubview(sliderView)
         
-        NSLayoutConstraint(item: sliderView, attribute: .top, relatedBy: .equal, toItem: containerView, attribute: .top, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: sliderView, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1, constant: 0).isActive = true
         
-        NSLayoutConstraint(item: sliderView, attribute: .leading, relatedBy: .equal, toItem: containerView, attribute: .leading, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: sliderView, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leading, multiplier: 1, constant: 0).isActive = true
         
-        NSLayoutConstraint(item: sliderView, attribute: .trailing, relatedBy: .equal, toItem: containerView, attribute: .trailing, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint(item: sliderView, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailing, multiplier: 1, constant: 0).isActive = true
         
         NSLayoutConstraint(item: sliderView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 100).isActive = true
         
